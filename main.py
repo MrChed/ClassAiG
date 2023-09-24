72d51b57190b4238350b21ec7bc1752eb533117a
_MrChed_ <141160614+MrChed@users.noreply.github.com>
2023-09-24 20:04:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import telebot
+import requests
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+
+# Инициализация бота
+bot = telebot.TeleBot('Api-key')
+
+# Загрузка предобученной модели ResNet50
+model = ResNet50(weights='imagenet')
+
+
+# Обработчик фотографий
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    # Получение фотографии из сообщения
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    file_url = f'https://api.telegram.org/file/bot{bot.token}/{file_info.file_path}'
+    response = requests.get(file_url)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+
+    # Предобработка изображения
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    # Классификация изображения
+    preds = model.predict(x)
+    decoded_preds = decode_predictions(preds, top=3)[0]
+
+    # Формирование сообщения с результатами классификации
+    message_text = 'Результаты классификации:\n'
+    for pred in decoded_preds:
+        message_text += f'{pred[1]} с вероятностью {pred[2]:.2%}\n'
+
+    # Отправка сообщения с результатами классификации
+    bot.reply_to(message, message_text)
+
+
+# Запуск бота
+bot.polling()
